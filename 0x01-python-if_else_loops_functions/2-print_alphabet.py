af8ec2584bfccc960d05bc5b6ddf1d979e445594
sunnywizy <sundayobasi222@gmail.com>
2023-11-28 14:33:27 +0100

    I just updated my 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A program that proints the ASCII alphabet, 
+"""A program that proints the ASCII alphabet,
 in lowercase, not followed by a new line."""
 for letters in range(97, 123):
     print("{}".format(chr(letters)), end="")
