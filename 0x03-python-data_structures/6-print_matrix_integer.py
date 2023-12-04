0c738b77dc2d4e6741b6b7f6b4040eb13095d214
sunnywizy <sundayobasi222@gmail.com>
2023-12-04 08:15:24 +0100

    I just added some files

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for rows in matrix:
+        for cols in rows:
+            print("{:d}".format(cols), end=" " if cols != rows[-1] else "")
+        print()
+    
