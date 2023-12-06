5dd4d1e3bdac354614550e0e2ed1f6227534fa6b
sunnywizy <sundayobasi222@gmail.com>
2023-12-06 18:17:37 +0100

    I just craete some files

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,9 +4,7 @@ def weight_average(my_list=[]):
         return 0
     num = 0
     den = 0
-
     for tup in my_list:
         num += tup[0] * tup[1]
         den += tup[1]
-        
     return (num / den)
