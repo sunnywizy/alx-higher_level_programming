9271685f2cb5212122a9e399c6ff55d589081137
sunnywizy <sundayobasi222@gmail.com>
2023-12-18 16:57:48 +0100

    I just corrected something in files

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -13,7 +13,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            ret += 1
+            count += 1
         except (ValueError, TypeError):
             continue
     print("")
