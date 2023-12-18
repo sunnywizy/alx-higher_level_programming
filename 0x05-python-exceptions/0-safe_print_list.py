18c146dae55bb363f6e887ffae658429322dc3fd
sunnywizy <sundayobasi222@gmail.com>
2023-12-18 16:56:16 +0100

    I just corrected something in files

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
     Returns:
         The number of elements printed.
     """
-    ret = 0
+    count = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
@@ -17,4 +17,4 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             break
     print("")
-    return (ret)
+    return (count)
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    """Returns the division of a by b."""
     try:
         num = a / b
-    except:
+    except (TypeError, ZeroDivisionError):
         num = None
     finally:
         print("Inside result: {}".format(num))
-        return num
+    return (num)
