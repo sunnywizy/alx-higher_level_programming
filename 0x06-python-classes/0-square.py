a7e5e76749bf797eab4cbbe6ae4bd4baf0e2b5e8
sunnywizy <sundayobasi222@gmail.com>
2023-12-19 15:24:45 +0100

    I just created some files

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+# 0-square.py
+# Obasi Sunday <sundayobasi222@gmail.com>
+"""Define a class Square."""
+
+
 class Square:
-    """Represent a square"""
+    """Represent a square."""
     pass
