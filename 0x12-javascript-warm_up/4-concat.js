0bc267bf491f4a97b3cb4123900b4ff9a1efc61f
sunnywizy <sundayobasi222@gmail.com>
2024-03-12 02:47:50 +0100

    I just created some files to my repository

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
 
-// Check if at least two arguments are passed
-if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-} else {
-  console.log('Not enough arguments provided');
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
