623f712da11f67e33e72bea39cf1d047893fa074
sunnywizy <sundayobasi222@gmail.com>
2024-03-12 02:35:48 +0100

    I just created some files to my repository

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Check the number of arguments passed
+const numOfArgs = process.argv.length - 2; 
+
+if (numOfArgs === 0) {
+    console.log("No argument");
+} else if (numOfArgs === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
+
