24aba279c7713a02ba90231de70004bbddb02979
sunnywizy <sundayobasi222@gmail.com>
2024-03-12 02:27:02 +0100

    I just created some files to my repository

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const firstLetter = 'C is fun';
+const secondLetter = 'Python is cool';
+const thirdLetter = 'JavaScript is amazing';
+console.log(firstLetter);
+console.log(secondLetter);
+console.log(thirdLetter);
