b9627318d77296ee7d5f24363821307b159f71ac
sunnywizy <sundayobasi222@gmail.com>
2024-03-12 03:07:00 +0100

    I just created some files to my repository

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 
 function add (a, b) {
-  console.log(a + b);
-}
-
-const firstInteger = parseInt(process.argv[2]);
-const secondInteger = parseInt(process.argv[3]);
-
-if (!isNaN(firstInteger) && !isNaN(secondInteger)) {
-  add(firstInteger, secondInteger);
-} else {
-  console.log('Please provide two valid integers.');
-}
+    return a + b;
+  }
+  
+  const a = parseInt(process.argv[2]);
+  const b = parseInt(process.argv[3]);
+  
+  console.log(add(a, b));
+  
