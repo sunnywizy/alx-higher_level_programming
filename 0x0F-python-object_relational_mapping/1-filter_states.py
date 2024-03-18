773568b5c35363271f3b10c6ae663a712903a259
sunnywizy <sundayobasi222@gmail.com>
2024-03-18 17:50:45 +0100

    I just created some files to my repository

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,14 +13,11 @@ if __name__ == '__main__':
     Access to the database and get the states
     from the database.
     """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    database = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY 'N%' \
-                 ORDER BY states.id ASC")
-    rows = cur.fetchall()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states \ WHERE name LIKE BINARY 'N%' \ ORDER BY states.id ASC")
+    rows = cursor.fetchall()
 
     for row in rows:
         print(row)
